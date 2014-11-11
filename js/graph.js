63dd9602d3ffdd5f2e6819c1e600e05d5b08ddf7
Faruk Sahin <fsahin@spotify.com>
2014-11-11 01:15:04 +0100

    Initial commit

---
+++ js/graph.js
@@ -0,0 +1,88 @@
+var width = 800,
+    height = 600;
+
+
+function update(links, nodes) {
+
+  d3.select("svg").remove();
+
+  var svg = d3.select("#graph-container").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  // var nodes = {};
+    // Compute the distinct nodes from the links.
+  links.forEach(function(link) {
+    link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
+    link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
+  });
+
+  var force = d3.layout.force()
+      .nodes(d3.values(nodes))
+      .links(links)
+      .size([width, height])
+      .linkDistance(150)
+      .charge(-3000)
+      .on("tick", tick)
+      .start();
+
+  // Per-type markers, as they don't inherit styles.
+  svg.append("defs").selectAll("marker")
+      .data(["suit", "licensing", "resolved"])
+    .enter().append("marker")
+      .attr("id", function(d) { return d; })
+      .attr("viewBox", "0 -5 10 10")
+      .attr("refX", 15)
+      .attr("refY", -1.5)
+      .attr("markerWidth", 6)
+      .attr("markerHeight", 6)
+      .attr("orient", "auto")
+    .append("path")
+      .attr("d", "M0,-5L10,0L0,5");
+
+  var path = svg.append("g").selectAll("path")
+      .data(force.links())
+    .enter().append("path")
+      .attr("class", function(d) { return "link " + d.type; })
+      .attr("marker-end", function(d) { return "url(#" + d.type + ")"; });
+
+  var circle = svg.append("g").selectAll("circle")
+      .data(force.nodes())
+    .enter().append("circle")
+      .attr("r", 15)
+      .call(force.drag);
+
+  var text = svg.append("g").selectAll("text")
+      .data(force.nodes())
+    .enter().append("text")
+      .attr("x", -2)
+      .attr("y", ".31em")
+      .text(function(d) { return d.name; });
+
+  function tick() {
+    path.attr("d", linkArc);
+    circle.attr("transform", transform);
+    text.attr("transform", transform);
+  }
+
+  function linkArc(d) {
+    var deltaX = d.target.x - d.source.x,
+        deltaY = d.target.y - d.source.y,
+        dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY),
+        normX = deltaX / dist,
+        normY = deltaY / dist,
+        sourcePadding = d.left ? 17 : 12,
+        targetPadding = d.right ? 17 : 12,
+        sourceX = d.source.x + (sourcePadding * normX),
+        sourceY = d.source.y + (sourcePadding * normY),
+        targetX = d.target.x - (targetPadding * normX),
+        targetY = d.target.y - (targetPadding * normY);
+    return 'M' + sourceX + ',' + sourceY + 'L' + targetX + ',' + targetY;
+  }
+
+  function transform(d) {
+    return "translate(" + d.x + "," + d.y + ")";
+  }
+
+}
+
---
+++ js/main.js
@@ -0,0 +1,93 @@
+G = {
+  "0": ["1", "3"],
+  "1": ["3", "4"],
+  "2": [],
+  "3": ["2"],
+  "4": ["3"],
+  "5": ["0"],
+}
+
+var editor = ace.edit("graph-input");
+editor.renderer.setShowGutter(false);
+editor.getSession().setMode("ace/mode/javascript");
+
+setTextAreaDefault();
+$("body").focus();
+
+function prettyPrintRefresh() {
+  var ugly = editor.getValue();
+  var obj = JSON.parse(ugly);
+  var pretty = js_beautify(JSON.stringify(obj, undefined));
+  editor.setValue(pretty);
+}
+
+function setTextAreaDefault() {
+  var pretty = js_beautify(JSON.stringify(G, undefined));
+  editor.setValue(pretty, -1);
+  $('#')
+}
+
+
+function nodeExists(nodes, val) {
+  var exists = false;
+  if (nodes[val.toString()]) {
+    exists = true;
+  }
+  return exists;
+}
+
+function createNode(id) {
+  var node = {}
+  node[id.toString()] = {
+    "name": id.toString()
+  }
+  return node;
+}
+
+function createData(G) {
+  var nodes = {};
+  var edges = [];
+
+  $.each(G, function(fromVertex, connectedVertices) {
+    if (!nodeExists(nodes, fromVertex)) {
+      nodes[fromVertex.toString()] = {
+        "name": fromVertex.toString()
+      }
+    }
+
+    connectedVertices.forEach(function(toVertex) {
+      if (!nodeExists(nodes, toVertex)) {
+        nodes[toVertex.toString()] = {
+          "name": toVertex.toString()
+        }
+      }
+
+      edge = {
+        "source": fromVertex.toString(),
+        "target": toVertex.toString(),
+        "type": "licensing",
+      }
+
+      edges.push(edge);
+    });
+  });
+
+
+  data = {
+    "nodes": nodes,
+    "edges": edges
+  }
+  return data;
+}
+
+data = createData(G);
+update(data.edges, data.nodes);
+
+
+function submit() {
+  val = editor.getValue();
+  console.log(val);
+  graph = JSON.parse(val);
+  data = createData(graph);
+  update(data.edges, data.nodes);
+}
